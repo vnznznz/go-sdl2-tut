362d477253f9205162d43c4d16919b9d0a8544ad
Vinzenz Sinapius <vinzenz.sinapius@gmail.com>
2017-02-10 18:56:51 +0100

    Port of "Getting an Image on the Screen"

---
+++ 02_FirstImage/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"log"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+var (
+	err           error
+	window        *sdl.Window
+	screenSurface *sdl.Surface
+	helloWorld    *sdl.Surface
+)
+
+func initSDL() error {
+	err = sdl.Init(sdl.INIT_VIDEO)
+
+	if err != nil {
+		return err
+	}
+
+	window, err = sdl.CreateWindow("SDL Tutorial", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, screenWidth, screenHeight, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return err
+	}
+
+	screenSurface, err = window.GetSurface()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadMedia() error {
+	helloWorld, err = sdl.LoadBMP("hello_world.bmp")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func close() {
+	helloWorld.Free()
+	window.Destroy()
+	sdl.Quit()
+}
+
+func main() {
+	err = initSDL()
+	if err != nil {
+		log.Fatal("Error initializing SDL:", err)
+	}
+
+	err = loadMedia()
+	if err != nil {
+		log.Fatal("Error initializing SDL:", err)
+	}
+
+	helloWorld.Blit(nil, screenSurface, nil)
+	window.UpdateSurface()
+	sdl.Delay(2000)
+
+	close()
+}
